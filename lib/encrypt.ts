4a58c68211288d7ce0512f2ad52132f20013420d
sean-birmingham <sean.birmingham98@gmail.com>
2025-01-11 17:03:00 -0500

    Refactor encryption methods to use SHA-256 hashing with salt and remove key-based dependency

--- lib/encrypt.ts
+++ lib/encrypt.ts
@@ -1,26 +1,16 @@
 const encoder = new TextEncoder();
-const key = new TextEncoder().encode(process.env.ENCRYPTION_KEY);
+const salt = crypto.getRandomValues(new Uint8Array(16)).join("");
 
-// Hash function with key-based encryption
+// Hash function
 export const hash = async (plainPassword: string): Promise<string> => {
-	const passwordData = encoder.encode(plainPassword);
-
-	const cryptoKey = await crypto.subtle.importKey(
-		"raw",
-		key,
-		{ name: "HMAC", hash: { name: "SHA-256" } },
-		false,
-		["sign", "verify"]
-	);
-
-	const hashBuffer = await crypto.subtle.sign("HMAC", cryptoKey, passwordData);
-
+	const passwordData = encoder.encode(plainPassword + salt);
+	const hashBuffer = await crypto.subtle.digest("SHA-256", passwordData);
 	return Array.from(new Uint8Array(hashBuffer))
 		.map((b) => b.toString(16).padStart(2, "0"))
 		.join("");
 };
 
-// Compare function using key from env var
+// Compare function
 export const compare = async (
 	plainPassword: string,
 	encryptedPassword: string
