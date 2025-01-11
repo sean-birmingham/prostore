efdbb58d716051ff547bdf846a26846e9233df65
sean-birmingham <sean.birmingham98@gmail.com>
2025-01-11 16:52:23 -0500

    Enhance session management by updating cart assignment logic and refining cookie handling

--- auth.ts
+++ auth.ts
@@ -33,7 +33,7 @@ export const config = {
 					},
 				});
 
-				// Check if user exists and if password matches
+				// Check if user exists and if the password matches
 				if (user && user.password) {
 					const isMatch = await compare(
 						credentials.password as string,
@@ -69,15 +69,15 @@ export const config = {
 
 			return session;
 		},
-
 		async jwt({ token, user, trigger, session }: any) {
-			// Assign user fields to the token
+			// Assign user fields to token
 			if (user) {
+				token.id = user.id;
 				token.role = user.role;
 
 				// If user has no name then use the email
 				if (user.name === "NO_NAME") {
-					token.name = user.email.split("@")[0];
+					token.name = user.email!.split("@")[0];
 
 					// Update database to reflect the token name
 					await prisma.user.update({
@@ -85,15 +85,61 @@ export const config = {
 						data: { name: token.name },
 					});
 				}
+
+				if (trigger === "signIn" || trigger === "signUp") {
+					const cookiesObject = await cookies();
+					const sessionCartId = cookiesObject.get("sessionCartId")?.value;
+
+					if (sessionCartId) {
+						const sessionCart = await prisma.cart.findFirst({
+							where: { sessionCartId },
+						});
+
+						if (sessionCart) {
+							// Delete current user cart
+							await prisma.cart.deleteMany({
+								where: { userId: user.id },
+							});
+
+							// Assign new cart
+							await prisma.cart.update({
+								where: { id: sessionCart.id },
+								data: { userId: user.id },
+							});
+						}
+					}
+				}
+			}
+
+			// Handle session updates
+			if (session?.user.name && trigger === "update") {
+				token.name = session.user.name;
 			}
 
 			return token;
 		},
 		authorized({ request, auth }: any) {
+			// Array of regex patterns of paths we want to protect
+			const protectedPaths = [
+				/\/shipping-address/,
+				/\/payment-method/,
+				/\/place-order/,
+				/\/profile/,
+				/\/user\/(.*)/,
+				/\/order\/(.*)/,
+				/\/admin/,
+			];
+
+			// Get pathname from the req URL object
+			const { pathname } = request.nextUrl;
+
+			// Check if user is not authenticated and accessing a protected path
+			if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+
 			// Check for session cart cookie
-			if (!request.cookies.get("sessionCartID")) {
+			if (!request.cookies.get("sessionCartId")) {
 				// Generate new session cart id cookie
-				const sessionCartID = crypto.randomUUID();
+				const sessionCartId = crypto.randomUUID();
 
 				// Clone the req headers
 				const newRequestHeaders = new Headers(request.headers);
@@ -105,8 +151,8 @@ export const config = {
 					},
 				});
 
-				// Set newly generated sessionCartID in the response cookies
-				response.cookies.set("sessionCartID", sessionCartID);
+				// Set newly generated sessionCartId in the response cookies
+				response.cookies.set("sessionCartId", sessionCartId);
 
 				return response;
 			} else {
