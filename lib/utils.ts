dd63f68d8782d80884dfa555689e651509fbd82c
sean-birmingham <sean.birmingham98@gmail.com>
2025-01-18 17:26:48 -0500

    Enhance PrismaClient with custom result transformers for cart and order pricing; add utility functions for ID formatting and date/time formatting

--- lib/utils.ts
+++ lib/utils.ts
@@ -69,3 +69,54 @@ export function formatCurrency(amount: number | string | null) {
 		return "NaN";
 	}
 }
+
+// Shorten UUID
+export function formatId(id: string) {
+	return `$..${id.substring(id.length - 6)}`;
+}
+
+// Format date and times
+export const formatDateTime = (dateString: Date) => {
+	const dateTimeOptions: Intl.DateTimeFormatOptions = {
+		month: "short",
+		year: "numeric",
+		day: "numeric",
+		hour: "numeric",
+		minute: "numeric",
+		hour12: true,
+	};
+
+	const dateOptions: Intl.DateTimeFormatOptions = {
+		weekday: "short",
+		month: "short",
+		year: "numeric",
+		day: "numeric",
+	};
+
+	const timeOptions: Intl.DateTimeFormatOptions = {
+		hour: "numeric",
+		minute: "numeric",
+		hour12: true,
+	};
+
+	const formattedDateTime: string = new Date(dateString).toLocaleString(
+		"en-US",
+		dateTimeOptions
+	);
+
+	const formattedDate: string = new Date(dateString).toLocaleString(
+		"en-US",
+		dateOptions
+	);
+
+	const formattedTime: string = new Date(dateString).toLocaleString(
+		"en-US",
+		timeOptions
+	);
+
+	return {
+		dateTime: formattedDateTime,
+		dateOnly: formattedDate,
+		timeOnly: formattedTime,
+	};
+};
