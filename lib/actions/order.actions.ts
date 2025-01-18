29c4ff2c1483268ed80fd946938a4d61d2b679f9
sean-birmingham <sean.birmingham98@gmail.com>
2025-01-18 17:04:13 -0500

    Add Order Details page and fetch order by ID; update order items field in schema

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { isRedirectError } from "next/dist/client/components/redirect-error";
-import { formatError } from "../utils";
+import { convertToPlainObject, formatError } from "../utils";
 import { auth } from "@/auth";
 import { getMyCart } from "./cart.actions";
 import { getUserById } from "./user.actions";
@@ -104,4 +104,19 @@ export async function createOrder() {
 
 		return { success: false, error: formatError(error) };
 	}
+}
+
+// Get order by id
+export async function getOrderById(orderId: string) {
+	const data = await prisma.order.findFirst({
+		where: { id: orderId },
+		include: {
+			orderitems: true,
+			user: {
+				select: { name: true, email: true },
+			},
+		},
+	});
+
+	return convertToPlainObject(data);
 }
