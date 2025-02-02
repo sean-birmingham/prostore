8efef496e78e0b2e06794ba40d01a216d35785de
sean-birmingham <sean.birmingham98@gmail.com>
2025-02-02 15:19:30 -0500

    Add admin layout and navigation; implement overview page and user role checks

--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -68,6 +68,16 @@ const UserButton = async () => {
 						</Link>
 					</DropdownMenuItem>
 
+					{session?.user?.role === "admin" && (
+						<DropdownMenuItem>
+							<Link
+								href="/admin/overview"
+								className="w-full">
+								Admin
+							</Link>
+						</DropdownMenuItem>
+					)}
+
 					<DropdownMenuItem className="p-0 mb-1">
 						<form
 							action={signOutUser}
